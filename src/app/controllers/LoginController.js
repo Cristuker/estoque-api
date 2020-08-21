4da9089947f39e95353e7815c645e0ed8c769d0f
Cristian Silva <cristian123105@gmail.com>
2020-08-21 16:27:15 -0300

    refactor: move all code controller into a try catch

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -5,31 +5,36 @@ import authConfig from '../../config/auth';
 
 class Login {
   async store(request, response) {
-    const schema = Yup.object({
-      email: Yup.string().required(),
-      password: Yup.string().required(),
-    });
+    try {
+      const schema = Yup.object({
+        email: Yup.string().required(),
+        password: Yup.string().required(),
+      });
 
-    if (!(await schema.isValid(request.body))) {
-      return response
-        .status(406)
-        .send({ message: 'The request body is not valid, check the params' });
+      if (!(await schema.isValid(request.body))) {
+        return response
+          .status(406)
+          .send({ message: 'The request body is not valid, check the params' });
+      }
+
+      const { email, password } = request.body;
+      const user = await Users.findOne({ where: { email } });
+
+      if (!user) {
+        return response.status(404).send({ message: 'User not find' });
+      }
+
+      if (!(await user.checkPassword(password))) {
+        return response.status(400).send({ message: 'Password dont match' });
+      }
+
+      const token = jwt.sign(user.id, authConfig.secret);
+
+      return response.send({ token });
+    } catch (error) {
+      console.error(error);
+      return response.status(500).send({ message: 'Internal server error' });
     }
-
-    const { email, password } = request.body;
-    const user = await Users.findOne({ where: { email } });
-
-    if (!user) {
-      return response.status(404).send({ message: 'User not find' });
-    }
-
-    if (!(await user.checkPassword(password))) {
-      return response.status(400).send({ message: 'Password dont match' });
-    }
-
-    const token = jwt.sign(user.id, authConfig.secret);
-
-    return response.send({ token });
   }
 }
 
